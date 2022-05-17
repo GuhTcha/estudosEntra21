19e05bce132d2c0f93895051231c10c4cc963e03
GuhTcha <102043948+GuhTcha@users.noreply.github.com>
2022-05-17 20:14:27 -0300

    até o 34

---
+++ e-python/e026.py
@@ -0,0 +1,7 @@
+var = 0
+#enquanto
+while var <= 10:
+    print(var)
+    var = var + 1
+
+print("fim")
---
+++ e-python/e029.py
@@ -0,0 +1,5 @@
+inicio = int(input("digite valor para o inicio"))
+fim = int(input('digite o valor do fim'))
+
+for i in range(inicio,fim+1):
+    print(i)
