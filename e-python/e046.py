38c9f79f4556c801bf97cd7ac29e10d62cef86a9
GuhTcha <102043948+GuhTcha@users.noreply.github.com>
2022-05-23 19:24:01 -0300

    e.046.py

--- e-python/e046.py
+++ e-python/e046.py
@@ -1,6 +1,8 @@
 no= input('Qual seu nome? ')
 i= input('Qual sua idade? ')
 s= input('Sexo? ')
-ec= input('estado civil ')
+ec= input('estado civil? ')
 n= input('Qual sua nacionalidade? ')
 r= input('Renda mensal? ')
+
+print (f'nome:{no}\nidade:{i}\nSexo:{s}\nEstado Civil:{ec}\nNacionalidade:{n}\nRenda Mensal:{r}\n\nSeus dados estão corretos?')
