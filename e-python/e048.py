b8c9aca541b70ce5928463aa2a7efc649781083e
GuhTcha <102043948+GuhTcha@users.noreply.github.com>
2022-05-23 22:14:08 -0300

    21/05/2022

--- e-python/e048.py
+++ e-python/e048.py
@@ -1,45 +1,7 @@
 # e048.py
-base = { 
-    'Pergunta 01':{
-        'pergunta':'Quanto é 4 x 4?',
-        'alternativas':{
-                'a':'12',
-                'b':'24',
-                'c':'16',
-                'd':'20',
-            },
-        'resposta_certa':'c'
-    },
-    'Pergunta 02':{
-        'pergunta':'Quanto é 6/3',
-        'alternativas':{
-                'a':'2',
-                'b':'1',
-                'c':'3',
-                'd':'d',
-            },
-        'resposta_certa':'a'
-    },
-}
+per = (input("1)Quem é considerado o pai da aviação? \na)Silvio Santos\nb)Ratinho\nc)Seu Zé\nd)Alberto Santos Dumont"))
 
-respostas_certas = 0 
+if per == ('d'):
+    print('Você acertou!')
 
-for pkeys,pvalues in base.items():
-    print(f'{pkeys}:{pvalues["pergunta"]}')
-
-    for rkeys,rvalues in pvalues['alternativas'].items():
-        print(f'[{rkeys}]:{rvalues}')
-
-    resposta = input('Escolha uma alternativa: [a],[b],[c], ou [d]')
-    print('')
-    if resposta == pvalues['resposta_certa']:
-        print('Resposta Correta!!!')
-    else:
-        print('Resposta ERRADA')
-
-if respostas_certas == 0:
-    print('Voce não acertou nenhuma questão!')
-elif respostas_certas == 1:
-    print('Você acertou apenas uma questão')
-else: 
-    print(f'Você acertou {respostas_certas}. Parabens!!!!')
+perg = (input("2)Quem inventou as mais importantes leis da física? \na)Joelma \nb)Renato Cariani \n \n"))
